6d04934086f45c711f6d32233090c3cbb3980590
Isac Larsson <isaclarsson98@gmail.com>
2021-08-23 22:10:20 +0200

    Create form and get user playlists on submit

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,7 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styled from '@emotion/styled';
 import { Global, css } from '@emotion/react';
+import {  getUserPlaylists} from '../functions/spotify';
 
 const Wrapper = styled('div')`
     width: 100%;
@@ -10,15 +11,31 @@ const Wrapper = styled('div')`
 `;
 
 const App = () => {
+    const [state, setState] = useState({
+        userId: '',
+    });
     // Save user id as cookie
     // Save selected playlist ids as cookie
     // Ask user if we should load earlier given information
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        const userPlaylists = await getUserPlaylists(state.userId);
+
+        if (userPlaylists) {
+            setState({ ...state, playlists: userPlaylists });
+        }
+
+    };
 
     return (
         <>
             <Global styles={css`body { margin: 0; }`} />
             <Wrapper>
-                
+                <form onSubmit={handleSubmit}>
+                    <input value={state.userId} onChange={(e) => setState({ ...state, userId: e.target.value })} />
+                    <button type="submit">OK!</button>
+                </form>
             </Wrapper>
         </>
     );
