8cb6bdedafc91b6a8924a4c674499d4cb999ede2
Isac Larsson <isaclarsson98@gmail.com>
2021-09-04 10:18:24 +0200

    Get and loop playlists

--- src/pages/Frontpage.js
+++ src/pages/Frontpage.js
@@ -1,21 +1,44 @@
 import React, { useEffect, useState } from 'react'
 import PropTypes from 'prop-types'
 import styled from '@emotion/styled';
-import { authorize, getRefreshToken, getUser } from 'functions/spotify';
+import { authorize, getRefreshToken, getUser, getUserPlaylists, getUserPlaylistsById } from 'functions/spotify';
 import Cookies from 'js-cookie';
+import { checkForToken } from 'functions/utils';
 
 const Wrapper = styled('div')`
     display: flex;
     flex-direction: column;
     height: 100%;
     width: 100%;
+    padding: 16px;
 `;
 
-const Frontpage = props => {
+const Column = styled('div')`
+    display: flex;
+    flex-direction: column;
+    font-size: 20px;
+`;
+
+const Playlist = styled('div')`
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+    margin-bottom: 24px;
+
+    & > img {
+        width: 100px;
+        height: 100px;
+        margin-right: 8px;
+    }
+`;
+
+const Frontpage = () => {
     const [user, setUser] = useState();
+    const [token, setToken] = useState();
+    const [playlists, setPlaylists] = useState();
+
     useEffect(() => {
         const handleAuth = async (token) => {
-            console.log(token);
             if (token) {
                 const fetchedUser = await getUser(token);
                 
@@ -24,31 +47,56 @@ const Frontpage = props => {
         };
 
         const checkAuth = async () => {
-            const accessToken = Cookies.get('access-token');
-            const refreshToken = Cookies.get('refresh-token');
+            const accessToken = checkForToken();
+            setToken(accessToken);
 
             if (accessToken) {
                 handleAuth(accessToken);
-            } else if (refreshToken) {
-                const { newAccessToken, newRefreshToken, ...rest } = await getRefreshToken(refreshToken);
-                console.log(rest);
-
-                var in1Hour = 1/24;
-                Cookies.set('access-token', newAccessToken, { expires: in1Hour });
-                if (newRefreshToken) {
-                    Cookies.set('refresh-token', newRefreshToken, { expires: 5 });
-                }
-
-                handleAuth(newAccessToken);
             }
         }
 
-        checkAuth();
+        if (!user) {
+            checkAuth();
+        }
     }, []);
 
-    return user ? (
-        <div>WELCOME</div>
-    ) : (
+    const getPlaylists = async (userId) => {
+        let playlistObject;
+        if (userId && typeof userId === 'string') {
+            playlistObject = await getUserPlaylistsById(token, userId);
+        } else {
+            playlistObject = await getUserPlaylists(token);
+        }
+
+        if (playlistObject) {
+            const { href, items, limit, next, offset, previous, total } = playlistObject;
+
+            setPlaylists(items);
+        }
+    }
+
+    const hasCookie = Cookies.get('refresh-token');
+
+    return user?.id ? (
+        <Wrapper>
+            <h2>WELCOME {user.name}</h2>
+            {playlists ? (
+                <Column>
+                    {playlists.map(playlist => {
+                        const image = playlist.images?.[0]?.url;
+                        return (
+                            <Playlist key={playlist.id}>
+                                <img src={image} alt={playlists.name} />
+                                <span>{playlist.name}</span>
+                            </Playlist>
+                        )}
+                    )}
+                </Column>
+            ) : (
+                <button onClick={getPlaylists}>GET MY PLAYLISTS</button>
+            )}
+        </Wrapper>
+    ) : hasCookie ? <div>LOADING...</div> : (
         <div>
             <div>We need you to authorize with your spotify account</div>
             <button onClick={authorize}>AUTHORIZE</button>
