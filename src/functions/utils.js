e278fac8cf3a9b5a6f60eeb82d3504729c51f28d
Isac Larsson <isaclarsson98@gmail.com>
2021-09-04 10:18:13 +0200

    Create function to check for tokens

--- src/functions/utils.js
+++ src/functions/utils.js
@@ -1,3 +1,6 @@
+import Cookies from 'js-cookie';
+import { getRefreshToken } from './spotify';
+
 export const getParamCode = (location) => {
     const urlSearchParams = new URLSearchParams(location.search);
     const params = Object.fromEntries(urlSearchParams.entries());
@@ -8,4 +11,26 @@ export const getParamCode = (location) => {
     }
 
     return null;
+}
+
+export const checkForToken = () => {
+    const accessToken = Cookies.get('access-token');
+    if (accessToken) {
+        return accessToken;
+    } else {
+        const refreshToken = Cookies.get('refresh-token');
+        const { newAccessToken, newRefreshToken } = getRefreshToken(refreshToken);
+
+        if (newAccessToken) {
+            const in1Hour = 1/24;
+            Cookies.set('access-token', accessToken, { expires: in1Hour });
+            if (newRefreshToken) {
+                Cookies.set('refresh-token', refreshToken, { expires: 5 });
+            }
+
+            return newAccessToken;
+        }
+    }
+
+    window.location.href = `${window.location.origin}/authorize`;
 }
